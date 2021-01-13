13dae708c3af240e413825aeef1b04070f73cb6c
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-01-12 23:56:50 -0500

    redux async actions - implementing redux-thunk

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,15 @@
-import { CHANGE_SEARCHFIELD } from './constants';
-
-const initialState = {
+import { 
+    CHANGE_SEARCHFIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+} from './constants'
+ 
+const initialStateSearch = {
     searchField: ''
 }
 
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state=initialStateSearch, action={}) => {
    // console.log(action.type)
     switch(action.type) {
         case CHANGE_SEARCHFIELD:
@@ -13,3 +18,23 @@ export const searchRobots = (state=initialState, action={}) => {
             return state    
     }
 } 
+
+const initialStateRobots = {
+    isPending: false,
+    robots: [],
+    error: ''
+}
+
+export const requestRobots = (state=initialStateRobots, action={}) => {
+    switch(action.type) {
+        case REQUEST_ROBOTS_PENDING:
+            return Object.assign({}, state, { isPending: true })
+        case REQUEST_ROBOTS_SUCCESS:
+            return Object.assign({}, state, {robots: action.payload, isPending: false})
+        case REQUEST_ROBOTS_FAILED:
+            return Object.assign({}, state, {error: action.payload, isPending: false})
+        default:
+            return state
+    }
+}    
+        
